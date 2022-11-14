89b80b94e21cdc34cca3f9bfff3ad260ee36905a
Samuel Irvan Putra <59359381+nagarete@users.noreply.github.com>
2022-11-14 20:15:56 +0700

    bljr (arrow)function tp msh bngng return tnpa log

---
+++ cuboidFunction.js
@@ -0,0 +1,9 @@
+cuboidVolume = (length, width, height) => console.log(length * width * height);
+
+cuboidVolume(2, 3, 4);
+
+// function cuboidVolume(length, width, height) {
+//   return console.log(length * width * height);
+// }
+
+// cuboidVolume(2, 3, 4);
---
+++ functionPractice.js
@@ -0,0 +1,29 @@
+//without parameter
+sayThanks = () =>
+  console.log("Thank you for your purchase! We appreciate your business.");
+
+sayThanks();
+
+// function sayThanks(){
+//     console.log('Thank you for your purchase! We appreciate your business.')
+//   };
+
+//without arguments
+//   sayThanks();
+//   sayThanks();
+//   sayThanks();
+
+//with parameter
+sayThanks = (name) =>
+  console.log(
+    "Thank you for your purchase " + name + "! We appreciate your business."
+  );
+
+//with arguments
+sayThanks("Cole");
+
+// function sayThanks(name) {
+//     console.log('Thank you for your purchase '+ name + '! We appreciate your business.');
+//   }
+
+//   sayThanks('Cole');
