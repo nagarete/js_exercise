acb580876b79552701d39a786b6c484b794d16f4
Samuel Irvan Putra <59359381+nagarete@users.noreply.github.com>
2022-11-17 23:28:04 +0700

    belajar Slice dan subString

---
+++ slicePractice.js
@@ -0,0 +1,4 @@
+const num = [1, 2, 3, 4, 5];
+let a = num.slice(2, 5);
+//ambil array
+console.log(a);
---
+++ subString.js
@@ -0,0 +1,4 @@
+let name = "helaw";
+//ambil string
+let a = name.substring(1, 3);
+console.log(a);
