200cc3baf54408f543ada7ece292efc98c4f0d3a
Samuel Irvan Putra <59359381+nagarete@users.noreply.github.com>
2022-11-23 13:58:42 +0700

    debugging misNum

--- missingNumber.js
+++ missingNumber.js
@@ -1,16 +1,18 @@
 const N = 5;
-const arr = [2, 3, 1, 5];
+const arr = [2, 3, 1, 4];
 
 const getMissingNumber = (num, numList) => {
   numList.sort((a, b) => a - b);
-  let missNum = [];
-  for (let i = 1; i < num; i++) {
+  let missNum;
+  for (let i = 1; i <= num; i++) {
     if (i !== numList[i - 1]) {
-      missNum.push(i);
-      console.log(i);
-      console.log(missNum);
+      missNum = i;
+      break;
+      // console.log(i);
+      // console.log(missNum);
     }
   }
+  // console.log(numList);
   return missNum;
 };
 
