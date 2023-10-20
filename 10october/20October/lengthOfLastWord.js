d6e04288648b67ae0fc61fcc9d412b2e593cb08e
Samuel Irvan Putra <59359381+nagarete@users.noreply.github.com>
2023-10-20 23:40:41 +0700

    length of last word

---
+++ 10october/20October/lengthOfLastWord.js
@@ -0,0 +1,15 @@
+const lengthOfLastWord = (s) => {
+  let currentLength = 0;
+  const newS = s.trim();
+  for (let i = s.length - 1; i >= 0; i--) {
+    if (newS[i] !== " ") currentLength++;
+    else if (newS[i] == " ") break;
+  }
+  return currentLength;
+};
+
+console.log(lengthOfLastWord("   fly me   to   the moon  "));
+console.log(lengthOfLastWord("Hello World"));
+console.log(lengthOfLastWord("luffy is still joyboy"));
+
+//don't understand why even already trimmed, the fly me to the moon test case is failed.
