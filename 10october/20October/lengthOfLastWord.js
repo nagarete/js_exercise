f657570e280f8263156c2f1df4fd7d6f3e7d6143
Samuel Irvan Putra <59359381+nagarete@users.noreply.github.com>
2023-10-21 14:12:38 +0700

    fix Length Of Last Word problem

--- 10october/20October/lengthOfLastWord.js
+++ 10october/20October/lengthOfLastWord.js
@@ -1,10 +1,14 @@
 const lengthOfLastWord = (s) => {
   let currentLength = 0;
   const newS = s.trim();
-  for (let i = s.length - 1; i >= 0; i--) {
-    if (newS[i] !== " ") currentLength++;
-    else if (newS[i] == " ") break;
+  for (let i = newS.length - 1; i >= 0; i--) {
+    if (newS[i] !== " ") {
+      currentLength++;
+      // console.log(newS[i]);
+    } else if (newS[i] == " ") break;
   }
+  // s.trim().split(' ')[s.length - 1].length
+  // return s.trim().split(" ").reverse()[0].length;
   return currentLength;
 };
 
@@ -14,3 +18,4 @@ console.log(lengthOfLastWord("luffy is still joyboy"));
 
 //don't understand why even already trimmed, the fly me to the moon test case is failed.
 //https://leetcode.com/problems/length-of-last-word/
+//done
---
+++ 10october/21October/mergeSortedArray2.js
@@ -0,0 +1,9 @@
+const merge = (nums1, m, nums2, n) => {
+  //while loop
+  //for loop
+  //swap
+};
+console.log(merge([1, 2, 4, 5, 0, 0], 3, [3, 6, 7], 3));
+console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3));
+console.log(merge([1], 1, [], 0));
+console.log(merge([0], 0, [1], 1));
