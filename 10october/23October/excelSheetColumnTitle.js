663860cacd832942e3455b5a9359c3a703bad852
Samuel Irvan Putra <59359381+nagarete@users.noreply.github.com>
2023-10-23 15:48:48 +0700

    learn create map with loops, converting numbers to alphabet in excel

--- 10october/23October/excelSheetColumnTitle.js
+++ 10october/23October/excelSheetColumnTitle.js
@@ -1,15 +1,29 @@
-const convertToTitle = (columnNumber) => {};
+const convertToTitle = (columnNumber) => {
+  const numberToAlphabet = new Map();
+  const alphabets = "ZABCDEFGHIJKLMNOPQRSTUVWXY";
+  for (let i = 0; i < alphabets.length; i++) {
+    numberToAlphabet.set(i, alphabets[i]);
+  }
+  const arr = [];
 
-// a = 1
-// z = 26
-// aa = 27
-// az = 52
-//ba = 53
-//bz = 78
-//ca=79
+  while (columnNumber >= 1) {
+    const remainder = columnNumber % 26;
+    columnNumber = Math.floor(columnNumber / 26);
+    arr.push(remainder);
+  }
+  let ans = "";
+  for (let i = arr.length - 1; i >= 0; i--) {
+    ans += numberToAlphabet.get(arr[i]);
+  }
+  return ans;
+};
 
 console.log(convertToTitle(1));
 console.log(convertToTitle(28));
 console.log(convertToTitle(701));
+console.log(convertToTitle(1821));
+console.log(convertToTitle(26));
+
+//modulo (%) = remainder
 
 //https://leetcode.com/problems/excel-sheet-column-title/
