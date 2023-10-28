92d1597058cd61513510b9a7acdcdf64dab6f21b
Samuel Irvan Putra <59359381+nagarete@users.noreply.github.com>
2023-10-28 23:27:33 +0700

    intersection of two arrays, 2 loop using set

---
+++ 10october/28October/intersectionTwoArrays.js
@@ -0,0 +1,20 @@
+const intersectionOfTwoArrays = (nums1, nums2) => {
+  let sameSetNum = new Set();
+  for (let i = 0; i < nums1.length; i++) {
+    for (let j = 0; j < nums2.length; j++) {
+      if (nums1[i] === nums2[j]) sameSetNum.add(nums1[i]);
+    }
+  }
+  let sameArrNum = [];
+  for (let value of sameSetNum) {
+    sameArrNum.push(value);
+  }
+  return sameArrNum;
+};
+
+console.log(intersectionOfTwoArrays([1, 2, 2, 1], [2, 2]));
+console.log(intersectionOfTwoArrays([4, 9, 5], [9, 4, 9, 8, 4]));
+
+//On^2+n (?)
+
+//https://leetcode.com/problems/intersection-of-two-arrays/
