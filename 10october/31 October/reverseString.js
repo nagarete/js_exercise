98083d0c023f20d15808b68c6895e201a130b403
Samuel Irvan Putra <59359381+nagarete@users.noreply.github.com>
2023-10-31 22:39:26 +0700

    reverse string with loop

---
+++ 10october/31 October/reverseString.js
@@ -0,0 +1,14 @@
+const reverseString = (s) => {
+  let newS = [];
+  for (let i = s.length - 1; i >= 0; i--) {
+    newS.push(s[i]);
+  }
+  return newS;
+};
+
+console.log(reverseString(["h", "e", "l", "l", "o"]));
+console.log(reverseString(["H", "a", "n", "n", "a", "h"]));
+
+//it's supposed to be 0(1) but I did it with 0(n)
+
+//https://leetcode.com/problems/reverse-string/
